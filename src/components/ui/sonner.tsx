674f5d70ff973086f2f40dc164b77e60d277e20f
rockske8er <rockske8er@gmail.com>
2024-01-18 13:09:46 -0300

    fix: add theme for toast component

--- src/components/ui/sonner.tsx
+++ src/components/ui/sonner.tsx
@@ -1,11 +1,13 @@
-import { useTheme } from 'next-themes'
+// import { useTheme } from 'next-themes'
 import { Toaster as Sonner } from 'sonner'
+
+import { useTheme } from '@/providers/theme-provider'
 
 type ToasterProps = React.ComponentProps<typeof Sonner>
 
 const Toaster = ({ ...props }: ToasterProps) => {
+  //  const { theme = 'system' } = useTheme()
   const { theme = 'system' } = useTheme()
-
   return (
     <Sonner
       theme={theme as ToasterProps['theme']}
